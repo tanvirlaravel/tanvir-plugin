4b4d8d7f3d62ea8c30347277766800cef2615c5a
Tanvir <tanvirlaravel@gmail.com>
2025-01-05 09:47:52 +0600

    initial commit

---
+++ tanvir-plugin.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+ * Plugin Name:       A Tanvir Plugin
+ * Plugin URI:        www://tanvir-pluign.com
+ * Description:       Learning Plugin
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Tanvir
+ * Author URI:        https://tanvir.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       tanvir-plugin
+ */
+
+ echo "HI";
+ die();
